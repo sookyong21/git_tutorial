020b62de9d0a87706879503fdcab91c8e74cd9dd
Administrator <Administrator@pc-01>
2019-04-10 16:42:12 +0900

    hihi

---
+++ gitdemo/src/org/study/demo/GitDemo.java
@@ -0,0 +1,10 @@
+package org.study.demo;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// 깃허브로 올려집니다.
+
+	}
+
+}
---
+++ gitdemo/src/org/study/work/MyMainapp.java
@@ -0,0 +1,12 @@
+package org.study.work;
+
+public class MyMainapp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		System.out.println("안녕하세요");
+
+	}
+
+}
